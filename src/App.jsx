e6ca157acb50f1f6e0c8477b69f7248fb68fd4b8
abujaher5 <abujaher5120@gmail.com>
2024-03-16 12:04:42 +0600

    added primary setup and blogs.json file

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./components/Headers/Header";
+import Blogs from "./components/Headers/Blogs/Blogs";
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  );
+}
+
+export default App;
