b529ebcbbd7d17560d86876a4ecdeb40ac11144b
abujaher5 <abujaher5120@gmail.com>
2024-03-16 17:48:51 +0600

    install propTypes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
+import "./index.css";
 import Header from "./components/Headers/Header";
 import Blogs from "./components/Headers/Blogs/Blogs";
+import Bookmarks from "./components/Bookmarks/Bookmarks";
+
 function App() {
   return (
     <>
       <Header></Header>
-      <Blogs></Blogs>
+      <div className="md:flex justify-between items-center ">
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
     </>
   );
 }
