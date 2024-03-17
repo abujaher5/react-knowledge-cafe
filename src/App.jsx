7a9f8c7dd01a232f1568b103a72923c0bad58f2e
abujaher5 <abujaher5120@gmail.com>
2024-03-17 16:13:48 +0600

    added bookmark section

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,20 @@ import "./index.css";
 import Header from "./components/Headers/Header";
 import Blogs from "./components/Headers/Blogs/Blogs";
 import Bookmarks from "./components/Bookmarks/Bookmarks";
+import { useState } from "react";
 
 function App() {
+  const [bookmarks, setBookmarks] = useState([]);
+
+  const handleAddToBookmark = (blog) => {
+    console.log("bookmark adding soon");
+  };
+
   return (
     <>
       <Header></Header>
-      <div className="md:flex justify-between items-center ">
-        <Blogs></Blogs>
+      <div className="md:flex justify-between items-center  max-w-7xl mx-auto">
+        <Blogs handleAddToBookmark={handleAddToBookmark}></Blogs>
         <Bookmarks></Bookmarks>
       </div>
     </>
--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,53 @@
 import PropTypes from "prop-types";
-const Blog = ({ blog }) => {
-  console.log(blog);
-  return <div></div>;
+import { FaBookmark } from "react-icons/fa";
+const Blog = ({ blog, handleAddToBookmark }) => {
+  const { title, cover, author, posted_date, reading_time, hashtags } = blog;
+
+  return (
+    <div className="mb-20">
+      <img
+        className="rounded-xl w-full mb-8"
+        src={cover}
+        alt={`Cover picture`}
+      />
+      <div className="flex justify-between">
+        <div className="flex gap-4">
+          <img className="w-14" src="src/assets/images/boy1.png" alt="" />
+
+          <div className="space-y-3">
+            <h3 className="text-2xl">{author}</h3>
+            <p>{posted_date}</p>
+          </div>
+        </div>
+        <div>
+          <span>{reading_time}</span>
+
+          <button
+            onClick={() => handleAddToBookmark(blog)}
+            className="ml-2 text-2xl text-red-600"
+          >
+            <FaBookmark></FaBookmark>
+          </button>
+        </div>
+      </div>
+      <h2 className="text-4xl">{title}</h2>
+
+      <p>
+        {hashtags.map((hash, idx) => (
+          <span key={idx}>
+            <a href="" className="text-blue-800 ">
+              {" "}
+              #{hash}
+            </a>
+          </span>
+        ))}
+      </p>
+    </div>
+  );
 };
 
 Blog.propTypes = {
   blog: PropTypes.object.isRequired,
+  handleAddToBookmark: PropTypes.func,
 };
 export default Blog;
